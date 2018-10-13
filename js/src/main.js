0d5a2ac4ce8e11ae20060d83c581e9e669306ce3
David Allenby <hello@davidallenby.com>
2018-10-13 12:12:00 +1100

    Began building canvas for intro

--- js/src/main.js
+++ js/src/main.js
@@ -7,6 +7,7 @@ app.home = {
   init: function() {
     home = this;
     this.initContactMe();
+    this.introBackground();
   },
 
   // Spam prevention
@@ -22,6 +23,27 @@ app.home = {
       $('.panel-loading').fadeOut('slow');
     })
   },
+
+  introBackground: function() {
+    var canvas = document.getElementById('intro-bg');
+    var context = canvas.getContext('2d');
+    var width = canvas.width / 4;
+    var ratio = window.devicePixelRatio || 1;
+    
+    canvas.style.width = canvas.width +'px';
+    canvas.style.height = canvas.height +'px';
+    canvas.width *= ratio;
+    canvas.height *= ratio;
+    
+    context.setTransform(ratio,0,0,ratio,0,0);
+
+    context.strokeStyle = '#D2B48C';
+    context.rect(50,50,width,width);
+    context.stroke();
+  
+    
+
+  }
 }
 
 $(document).ready(function() {
