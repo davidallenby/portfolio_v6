c3923f5c022a5190f09275d585b4c40110152fd0
David Allenby <hello@davidallenby.com>
2019-01-20 18:14:01 +1100

    Began canvas animation for home screen

--- js/src/main.js
+++ js/src/main.js
@@ -7,7 +7,7 @@ app.home = {
   init: function() {
     home = this;
     this.initContactMe();
-    this.introBackground();
+    this.canvas();
   },
 
   // Spam prevention
@@ -24,25 +24,48 @@ app.home = {
     })
   },
 
-  introBackground: function() {
+  canvas: function() {
     var canvas = document.getElementById('intro-bg');
-    var context = canvas.getContext('2d');
-    var width = canvas.width / 4;
     var ratio = window.devicePixelRatio || 1;
-    
-    canvas.style.width = canvas.width +'px';
-    canvas.style.height = canvas.height +'px';
-    canvas.width *= ratio;
-    canvas.height *= ratio;
-    
-    context.setTransform(ratio,0,0,ratio,0,0);
+    var rect = canvas.getBoundingClientRect();
+    var context = canvas.getContext('2d');
 
-    context.strokeStyle = '#D2B48C';
-    context.rect(50,50,width,width);
-    context.stroke();
-  
-    
+    // Update canvas for Retina Displays
+    canvas.width = rect.width * ratio;
+    canvas.height = rect.height * ratio;
+    context.scale(ratio,ratio);
 
+    var squareSize = canvas.height / 6;
+    var x = 175;
+    var y = 200;
+    var move = 1;
+
+    function drawSquare() {
+      context.beginPath();
+      context.strokeStyle = '#D2B48C';
+      context.lineWidth = 1;
+      context.rect(x,y,squareSize,squareSize);
+      context.stroke();
+      context.closePath();
+    }
+
+    function draw() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      drawSquare();
+
+      
+      if(y + move > rect.height - squareSize || y + move < 0) {
+        move = -move;
+      }
+      if(x + move > rect.width - squareSize || x + move < 0) {
+        move = -move;
+      }
+
+      x += move;
+      y += move;
+    }
+    
+    setInterval(draw,10)
   }
 }
 
