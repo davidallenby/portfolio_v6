89088221f4ec650758f8e93322e06b4df6118b91
David Allenby <hello@davidallenby.com>
2020-03-26 20:38:00 +1100

    Content changes and animations

--- js/src/main.js
+++ js/src/main.js
@@ -7,22 +7,60 @@ app.home = {
   init: function() {
     home = this;
     this.initContactMe();
+    this.scrollToLink();
   },
 
-  // Spam prevention
+  /**
+   * Spam prevention. Overrides click event on "email me" links and fires mailto
+   * Prevents bots/spammers from seeing my email address.
+   */
   initContactMe: function() {
-    $('.cntct-me').attr({
-      'href':'mailto:hello@davidallenby.com',
-      'title': 'Get in touch'
-    })
+    const mailto = 'mailto:hello@davidallenby.com';
+    // On Click
+    $(document).on('click', '.cntct-me', function(e) {
+      e.preventDefault();
+      window.location.href = mailto;
+    });
+    // On Hover
+    $(document).on('mouseenter', '.cntct-me', function(e) {
+      e.target.href = mailto;
+    });
   },
 
+  /**
+   * Fade out spinner when page resources have loaded
+   */
   pageLoaded: function() {
     setTimeout(function() {
       $('.spinner').fadeOut('slow', function() {
         $('.panel-loading').fadeOut('slow');
       })
     }, 1000);
+  },
+
+  /**
+   * Fade in content on scroll.
+   */
+  fadeInContent: function() {
+    var triggerPoint = 
+      $(window).scrollTop() + $(window).innerHeight();
+
+    $('.fade-in-section').each(function() {
+      var contentTop = $(this).offset().top;
+
+      if (triggerPoint >= contentTop) $(this).addClass('is-visible');
+    });
+  },
+
+  scrollToLink: function() {
+    $(document).on('click', '.scroll-to', function(e) {
+      e.preventDefault();
+      var contentId = $(this).attr('href');
+
+      $('html, body').animate({
+        'scrollTop': $(contentId).offset().top - $('header').outerHeight()
+      })
+    })
   }
 }
 
@@ -32,4 +70,9 @@ $(document).ready(function() {
 
 window.onload = function() {
   app.home.pageLoaded();
+  app.home.fadeInContent();
 }
+
+$(window).scroll(function() {
+  app.home.fadeInContent();
+})
